e6fd43c7c9f70c9a3d0358aecf501f8e88672f59
Juho Kim <juho@juhokim.com>
2015-01-26 22:29:51 -0500

    migration to juhokim.com successful

--- learnersourcing/learnersourcing/settings.py
+++ learnersourcing/learnersourcing/settings.py
@@ -56,7 +56,7 @@ USE_TZ = True
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
 #MEDIA_ROOT = ''
-MEDIA_ROOT = '/srv/www/juhokim.com/public_html/crowdy/media/'
+MEDIA_ROOT = '/srv/www/crowdy.juhokim.com/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -68,7 +68,7 @@ MEDIA_URL = ''
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
 #STATIC_ROOT = '/var/www/learnersourcing/static/'
-STATIC_ROOT = '/srv/www/juhokim.com/public_html/crowdy/static/'
+STATIC_ROOT = '/srv/www/crowdy.juhokim.com/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
